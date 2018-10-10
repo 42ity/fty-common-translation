93a72aeb48a499ca5d2eb125c65366d408f08c41
Jiri Kukacka <JiriKukacka@Eaton.com>
2018-10-10 12:19:00 +0200

    Improved translation scripts

--- collect_translations.sh
+++ collect_translations.sh
@@ -7,14 +7,25 @@ if [[ ! -z $1 ]] ; then
 fi
 
 # .tsl = translation string list
-echo "COLLECTING TRANSLATIONS"
+echo "-== COLLECTING TRANSLATIONS ==-"
 # gather first argument of TRANSLATE_ME
-echo "" >"${output}.tsl"
-for files in $(grep -rsIl --include="*.c" --include="*.cc" --include="*.cpp" --include="*.ecpp" --include="*.h" --include="*.hpp" --include="*.inc" --exclude-dir=".build" --exclude-dir=".srcclone" --exclude-dir=".install" TRANSLATE_ME "${target}"); do tr -d '\n' <"${files}" | sed 's/#define *TRANSLATE_ME//g;s/TRANSLATE_ME *(/\n/g' | sed 's/\([^\]\)".*$/\1/' | sort | uniq >"${output}.tsl" ; done
+echo "" >"${output}.ttsl"
+for files in $(grep -rsIl --include="*.c" --include="*.cc" --include="*.cpp" --include="*.ecpp" --include="*.h" --include="*.hpp" --include="*.inc" --exclude-dir=".build" --exclude-dir=".srcclone" --exclude-dir=".install" TRANSLATE_ME "${target}"); do
+    echo "Searching ${files}"
+    tr -d '\n' <"${files}" | sed 's/#define *TRANSLATE_ME//g;s/TRANSLATE_ME *( *"" *)//g;s/TRANSLATE_ME *( *"/\n/g' | tail -n +2 | sed 's/\([^\]\)" *\(,\|)\).*$/\1/' >>"${output}.ttsl"
+    echo "" >>"${output}.ttsl"
+done
+sed '/^\s*$/d' "${output}.ttsl" | sort | uniq >"${output}.tsl"
 # gather whole content of TRANSLATE_LUA
-grep -rsnI --include="*.rule" --include="*.c" --include="*.cc" --include="*.cpp" --include="*.ecpp" --include="*.h" --include="*.hpp" --include="*.inc" --exclude-dir=".build" --exclude-dir=".srcclone" --exclude-dir=".install" "TRANSLATE_LUA *(" ${target} | grep -v '#define TRANSLATE_LUA *(' | sed 's/^.*TRANSLATE_LUA *(//' | sed 's/\([^\]\)).*$/\1/' | sort | uniq >"${output}_lua.tsl" # .tsl = translation string list
-if (grep "[^\\]\"" "${output}") ; then
-    echo "ERROR, UNESCAPED QUOTE \" CHARACTER FOUND, YOU NEED TO PERFORM MANUAL CHECK"
+grep -rsnI --include="*.rule" --include="*.c" --include="*.cc" --include="*.cpp" --include="*.ecpp" --include="*.h" --include="*.hpp" --include="*.inc" --exclude-dir=".build" --exclude-dir=".srcclone" --exclude-dir=".install" "TRANSLATE_LUA *(" ${target} | grep -v '#define TRANSLATE_LUA *(' | sed 's/^.*\(TRANSLATE_LUA *(\)/\1/' | sed 's/\([^\])\)\(\\\|\)\(\"\|\x27\).*$/\1/' | sort | uniq >"${output}_lua.tsl"
+echo "-== CLEAN UP ==-"
+rm "${output}.ttsl"
+if (grep "[^\\]\"" "${output}.tsl") ; then
+    echo "ERROR IN ${output}.tsl, UNESCAPED QUOTE \" CHARACTER FOUND, YOU NEED TO PERFORM MANUAL CHECK"
     exit 1
 fi
-echo "DONE, OUTPUT PUT TO ${output}"
+if (grep "[^\\]\"" "${output}_lua.tsl") ; then
+    echo "ERROR IN ${output}_lua.tsl, UNESCAPED QUOTE \" CHARACTER FOUND, YOU NEED TO PERFORM MANUAL CHECK"
+    exit 1
+fi
+echo "DONE, OUTPUT PUT TO ${output}.tsl AND ${output}_lua.tsl"
--- translations_to_weblate.sh
+++ translations_to_weblate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+MY_PATH="`dirname \"$0\"`"
 target='*.tsl'
 output='BE_weblate.json'
 if [[ ! -z $1 ]] ; then
@@ -7,17 +8,8 @@ fi
 if [[ ! -z $2 ]] ; then
     output=$2
 fi
-
-echo "CHANGING FORMAT TO WEBLATE"
-cat "${target}" > weblate_translations.tmp
-awk '
-        BEGIN { print "{"; }
-        {   if (NR==TNR)
-                {print "\t\""$0"\": \""$0"\"";}
-              else
-                  {print "\t\""$0"\": \""$0"\",";}
-        }
-        END { print "}" }
-    ' weblate_translations.tmp >>"${output}"
+echo "CHANGING FORMAT TO WEBLATE (json)"
+cat ${target} > weblate_translations.tmp
+awk -v TNR="$(wc -l "weblate_translations.tmp" | cut -d' ' -f1)" -f "${MY_PATH}/translations_to_weblate.awk" weblate_translations.tmp >"${output}"
 rm -f weblate_translations.tmp
 echo "DONE, OUTPUT PUT TO ${output}"
